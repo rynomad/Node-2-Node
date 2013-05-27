3e7ea05a72694e37a474df4d8f6f814d6058b70a
rynomad <ryan@nei.ghbor.net>
2013-05-27 14:01:50 -0600

    initial stack

---
+++ ccnx-0.7.2/csrc/conf.mk
@@ -0,0 +1,29 @@
+# generated Linux ccnlx1 2.6.32-45-server #104-Ubuntu SMP Tue Feb 19 21:35:01 UTC 2013
+#
+#
+#
+SHEXT=so
+SHLIBNAME=libccn.$(SHEXT).1
+SHLIBDEPS=
+SHARED_LD_FLAGS = -shared --whole-archive -soname=$(SHLIBNAME) -lc
+PLATCFLAGS=-fPIC
+CWARNFLAGS = -Wall -Wpointer-arith -Wreturn-type -Wstrict-prototypes
+CPREFLAGS= -I../include -D_REENTRANT
+COPT = -g
+INSTALL_BASE = /usr/local
+INSTALL_INCLUDE = $(INSTALL_BASE)/include
+INSTALL_LIB = $(INSTALL_BASE)/lib
+INSTALL_BIN = $(INSTALL_BASE)/bin
+PCAP_PROGRAMS = ccndumppcap
+RESOLV_LIBS = -lresolv
+ANT = /usr/bin/ant
+CP = cp
+INSTALL = install
+LS = /bin/ls
+RM = rm -f
+SH = /bin/sh
+MKDEP = gcc -MM
+BUILD_JAVA = true
+DINST_BIN = $(DESTDIR)$(INSTALL_BIN)
+DINST_INC = $(DESTDIR)$(INSTALL_INCLUDE)
+DINST_LIB = $(DESTDIR)$(INSTALL_LIB)
